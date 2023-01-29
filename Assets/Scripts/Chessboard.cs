5d188066e5b66ecc455e32beefb277ba129d0464
NKLam <khoalamvn@gmail.com>
2023-01-29 14:27:04 +1100

    Initial commit

---
+++ Assets/Scripts/Chessboard.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Chessboard : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        GenerateAllTiles(1, 8, 8);
+    }
+
+
+
+    private void GenerateAllTiles(float tileSize, int tileCountX, int tileCountY)
+    {
+
+    }
+}
