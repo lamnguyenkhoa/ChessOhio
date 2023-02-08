0b248a693be9baf47baff1f5edeb318490466505
NKLam <khoalamvn@gmail.com>
2023-02-08 14:58:54 +1100

    Add different camera for each player

--- Assets/Scripts/ChessPlayer.cs
+++ Assets/Scripts/ChessPlayer.cs
@@ -15,11 +15,19 @@ public class ChessPlayer : NetworkBehaviour
             // Host connection is set to true inside GameManager already
             team = PieceTeam.WHITE;
             playerName = "HosterPro2000";
+            GameObject.Find("Main Camera (White)").GetComponent<Camera>().enabled = true;
+            GameObject.Find("Main Camera (White)").GetComponent<AudioListener>().enabled = true;
+            GameObject.Find("Main Camera (Black)").GetComponent<Camera>().enabled = false;
+            GameObject.Find("Main Camera (Black)").GetComponent<AudioListener>().enabled = false;
         }
         else if (NetworkManager.Singleton.IsClient)
         {
             team = PieceTeam.BLACK;
             playerName = "Xx_LmaoClient_xX";
+            GameObject.Find("Main Camera (White)").GetComponent<Camera>().enabled = false;
+            GameObject.Find("Main Camera (White)").GetComponent<AudioListener>().enabled = false;
+            GameObject.Find("Main Camera (Black)").GetComponent<Camera>().enabled = true;
+            GameObject.Find("Main Camera (White)").GetComponent<AudioListener>().enabled = true;
             GameManager.getInstance().ClientConnectServerRpc();
         }
     }
