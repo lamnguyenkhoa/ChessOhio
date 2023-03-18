b79c8d21f0432b89ed71099d834d0dc76dfbca9b
NKLam <khoalamvn@gmail.com>
2023-03-18 16:26:01 +1100

    Improve mute bgm button
    Now carry over between scene

--- Assets/Scripts/MuteButton.cs
+++ Assets/Scripts/MuteButton.cs
@@ -7,17 +7,38 @@ public class MuteButton : MonoBehaviour
     public Button button;
     public AudioSource audioSource;
 
+    public void Start()
+    {
+        // Sync setting
+        if (!audioSource)
+            return;
+        if (GameSetting.instance.muteBGM)
+            Mute();
+        else
+            Unmute();
+    }
+
     public void OnMuteButton()
     {
-        if (audioSource)
-            audioSource.mute = !audioSource.mute;
+        if (!audioSource)
+            return;
         if (audioSource.mute)
-        {
-            button.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "Unmute";
-        }
+            Unmute();
         else
-        {
-            button.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "Mute";
-        }
+            Mute();
+    }
+
+    public void Mute()
+    {
+        audioSource.mute = true;
+        button.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "Unmute BGM";
+        GameSetting.instance.muteBGM = true;
+    }
+
+    public void Unmute()
+    {
+        audioSource.mute = false;
+        button.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "Mute BGM";
+        GameSetting.instance.muteBGM = false;
     }
 }
