30321136e1bb5240a213a588794f32d0f6456aff
NKLam <khoalamvn@gmail.com>
2023-04-02 01:10:43 +1100

    Add zooming for camera control

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,8 +4,12 @@ public class CameraController : MonoBehaviour
 {
     public Transform target; // The object to orbit around
     public float rotationSpeed = 5.0f;
-    private float distance;
-    private float mouseX, mouseY;
+    public float distance;
+    private float mouseX;
+    private float mouseY;
+    public float zoomSpeed = 2.0f;
+    public float minDistance = 2.0f;
+    public float maxDistance = 10.0f;
 
     void Start()
     {
@@ -14,6 +18,11 @@ public class CameraController : MonoBehaviour
         mouseY = transform.rotation.eulerAngles.x;
         Quaternion rotation = Quaternion.Euler(mouseY, mouseX, 0f);
         transform.position = target.position - (rotation * Vector3.forward * distance);
+
+        if (maxDistance < distance)
+        {
+            maxDistance = distance;
+        }
     }
 
 
@@ -29,14 +38,18 @@ public class CameraController : MonoBehaviour
             mouseX += Input.GetAxis("Mouse X") * rotationSpeed;
             mouseY -= Input.GetAxis("Mouse Y") * rotationSpeed;
             mouseY = Mathf.Clamp(mouseY, -90f, 90f);
-
-            // Calculate the new position of the camera based on the target object's position and the camera's distance and rotation
-            Vector3 targetPosition = target.position;
-            Quaternion rotation = Quaternion.Euler(mouseY, mouseX, 0f);
-            Vector3 cameraPosition = targetPosition - (rotation * Vector3.forward * distance);
-
-            transform.rotation = rotation;
-            transform.position = cameraPosition;
         }
+
+        // Zoom the camera in and out based on the mouse scroll wheel
+        distance -= Input.mouseScrollDelta.y * zoomSpeed;
+        distance = Mathf.Clamp(distance, minDistance, maxDistance);
+
+        // Calculate the new position of the camera based on the target object's position and the camera's distance and rotation
+        Vector3 targetPosition = target.position;
+        Quaternion rotation = Quaternion.Euler(mouseY, mouseX, 0f);
+        Vector3 cameraPosition = targetPosition - (rotation * Vector3.forward * distance);
+
+        transform.rotation = rotation;
+        transform.position = cameraPosition;
     }
 }
