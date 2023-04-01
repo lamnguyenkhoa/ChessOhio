efe627b96136ac2013aff2633236668268929eaa
NKLam <khoalamvn@gmail.com>
2023-04-02 01:03:57 +1100

    Add rotation for camera control

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target; // The object to orbit around
+    public float rotationSpeed = 5.0f;
+    private float distance;
+    private float mouseX, mouseY;
+
+    void Start()
+    {
+        distance = Vector3.Distance(transform.position, target.position);
+        mouseX = transform.rotation.eulerAngles.y;
+        mouseY = transform.rotation.eulerAngles.x;
+        Quaternion rotation = Quaternion.Euler(mouseY, mouseX, 0f);
+        transform.position = target.position - (rotation * Vector3.forward * distance);
+    }
+
+
+    void Update()
+    {
+        if (Chessboard.instance.disableRaycastCount > 0)
+        {
+            return;
+        }
+
+        if (Input.GetMouseButton(1)) // Check if the right mouse button is held down
+        {
+            mouseX += Input.GetAxis("Mouse X") * rotationSpeed;
+            mouseY -= Input.GetAxis("Mouse Y") * rotationSpeed;
+            mouseY = Mathf.Clamp(mouseY, -90f, 90f);
+
+            // Calculate the new position of the camera based on the target object's position and the camera's distance and rotation
+            Vector3 targetPosition = target.position;
+            Quaternion rotation = Quaternion.Euler(mouseY, mouseX, 0f);
+            Vector3 cameraPosition = targetPosition - (rotation * Vector3.forward * distance);
+
+            transform.rotation = rotation;
+            transform.position = cameraPosition;
+        }
+    }
+}
